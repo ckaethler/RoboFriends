a76147036e796de00d16ff0c8bd46486ed059e6f
ckaethler <kaethlec@oregonstate.edu>
2020-03-17 14:16:16 -0700

    Added Loading feature & api fetch for robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+// ----------------------------------------------------------------------------
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './Robots';
 import './App.css';
 
+// ----------------------------------------------------------------------------
 class App extends Component {
     constructor() {
         super() 
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
             }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -21,6 +28,10 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
+
+        if (filteredRobots.length === 0) {
+            return <h1>Loading...</h1>
+        }
 
         return (
             <div className="tc">
