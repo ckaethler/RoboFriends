11769517a82bb024120809685c5dd3999a3aa95b
ckaethler <kaethlec@oregonstate.edu>
2020-03-17 12:54:04 -0700

    Made CardList & reformatted

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import { robots } from './Robots';
 import 'tachyons';
+import CardList from './CardList';
 
 ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>,
+    <CardList robots={robots}/>,
     document.getElementById('root'));
