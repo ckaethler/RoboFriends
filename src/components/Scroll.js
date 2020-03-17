f76367e7f707cd3bb39615f9e21f78b7fbf657e1
ckaethler <kaethlec@oregonstate.edu>
2020-03-17 16:19:55 -0700

    Made Scroll window larger

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 // ----------------------------------------------------------------------------
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '200px'}}>
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '600px'}}>
             {props.children}
         </div>
     );
