4cea3d129a7af3e41b1a73abe598bb391bd556a3
ckaethler <kaethlec@oregonstate.edu>
2020-03-17 12:06:03 -0700

    Adding Card object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons'
+import Card from './Card';
+import 'tachyons';
 
-ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Card />, document.getElementById('root'));
