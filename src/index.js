bcc9a5092d75b0150e4d559408d02db9143b3795
ckaethler <catherinekaethler@gmail.com>
2020-04-23 23:21:05 -0700

    Added react-redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------------
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'redux';
+import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import 'tachyons';
