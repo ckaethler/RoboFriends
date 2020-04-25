e4ff4d57b2fd030972157c4aa330683393a386df
ckaethler <catherinekaethler@gmail.com>
2020-04-25 12:07:15 -0700

    finished putting through redux actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 // ---------------------------------------------------------------------------
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 
 // ---------------------------------------------------------------------------
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 // ---------------------------------------------------------------------------
 // by putting store in provider, makes accessible to all inside of it
--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,47 @@
-import { CHANGE_SEARCHFIELD } from './constants';
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from "./constants";
 
-const initialState = {
-    searchField: '',
-}
+const initialStateSearch = {
+  searchField: "",
+};
 
 // reducer is function
 // reducer gets state and action
 // if get action, act upon state
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCHFIELD:
             return Object.assign({}, state, {searchField: action.payload});
         default:
             return state;
     }
+};
+
+const InitialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: '',
 }
+
+export const requestRobots = (state = InitialStateRobots, action = {}) => {
+         switch (action.type) {
+           case REQUEST_ROBOTS_PENDING:
+             return Object.assign({}, state, { isPending: true });
+           case REQUEST_ROBOTS_SUCCESS:
+             return Object.assign({}, state, {
+               robots: action.payload,
+               isPending: true,
+             });
+           case REQUEST_ROBOTS_FAILED:
+             return Object.assign({}, state, {
+               error: action.payload,
+               isPending: false,
+             });
+           default:
+             return state;
+         }
+       };
